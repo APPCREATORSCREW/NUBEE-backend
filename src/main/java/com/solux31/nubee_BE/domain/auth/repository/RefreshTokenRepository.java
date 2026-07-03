35ef53b0386c272543b682ebb9bf9d956c59c13a
daeunkim701 <lionboot2025@gmail.com>
2026-07-03 18:42:44 +0900

    feat: RefreshTokenRepository 구현

--- src/main/java/com/solux31/nubee_BE/domain/auth/repository/RefreshTokenRepository.java
+++ src/main/java/com/solux31/nubee_BE/domain/auth/repository/RefreshTokenRepository.java
@@ -1,4 +1,12 @@
 package com.solux31.nubee_BE.domain.auth.repository;
 
-public interface RefreshTokenRepository {
+import com.solux31.nubee_BE.domain.auth.entity.RefreshToken;
+import com.solux31.nubee_BE.domain.auth.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByTokenHash(String tokenHash);
+    void deleteByUser(User user);
 }
