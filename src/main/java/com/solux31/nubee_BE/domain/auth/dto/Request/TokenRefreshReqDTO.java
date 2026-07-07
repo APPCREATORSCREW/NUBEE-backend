ccde3963236d78dc6118a86abd0be2ac0c1ab1d7
daeunkim701 <lionboot2025@gmail.com>
2026-07-07 23:31:19 +0900

    feat: 토큰 갱신 DTO 구현

---
+++ src/main/java/com/solux31/nubee_BE/domain/auth/dto/Request/TokenRefreshReqDTO.java
@@ -0,0 +1,13 @@
+package com.solux31.nubee_BE.domain.auth.dto.Request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class TokenRefreshReqDTO {
+
+    @NotBlank(message = "Refresh Token은 필수입니다.")
+    private String refreshToken;
+}
