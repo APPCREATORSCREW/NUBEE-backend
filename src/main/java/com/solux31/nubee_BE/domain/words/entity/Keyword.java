70cfee9448b70798c67bacedb85bc2607f23ebef
nylee0116 <specialnylee@gmail.com>
2026-07-07 11:16:11 +0900

    [feat] keyword entity 생성 #4

--- src/main/java/com/solux31/nubee_BE/domain/words/entity/Keyword.java
+++ src/main/java/com/solux31/nubee_BE/domain/words/entity/Keyword.java
@@ -1,4 +1,28 @@
 package com.solux31.nubee_BE.domain.words.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+
 public class Keyword {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long keywordId;
+
+    @Column(nullable = false, length = 50)
+    private String word; // 예: "우주선", "금리"
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String explanation; // 단어 뜻풀이 설명
+
+    @Column(nullable = false, length = 20)
+    private String keywordType; // "MAIN" 또는 "SUB" 구분
+
+    @Column(nullable = false)
+    private Long newsId; // 이 단어가 속한 뉴스 ID (DailyNews와 연결고리)
 }
