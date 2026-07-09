d6ea760be1353f23587d1c11ef9e733eb539d9a1
daeunkim701 <lionboot2025@gmail.com>
2026-07-09 18:48:53 +0900

    fix: is_verified 필드의 Default값에 대해 @Builder.Default 추가

--- src/main/java/com/solux31/nubee_BE/domain/auth/entity/EmailVerification.java
+++ src/main/java/com/solux31/nubee_BE/domain/auth/entity/EmailVerification.java
@@ -32,6 +32,7 @@ public class EmailVerification {
     @Column(name = "expires_at", nullable = false)
     private LocalDateTime expiresAt;
 
+    @Builder.Default
     @Column(name = "is_verified", nullable = false)
     private boolean isVerified = false;
 
