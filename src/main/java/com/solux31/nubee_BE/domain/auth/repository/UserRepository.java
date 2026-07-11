819a8f52b63197dc8c94e042f1cec2e71becd5d0
daeunkim701 <lionboot2025@gmail.com>
2026-07-12 00:04:15 +0900

    feat: existsByKakaoId 추가

--- src/main/java/com/solux31/nubee_BE/domain/auth/repository/UserRepository.java
+++ src/main/java/com/solux31/nubee_BE/domain/auth/repository/UserRepository.java
@@ -10,4 +10,5 @@ public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
 
     Optional<User> findByKakaoId(String kakaoId);
+    boolean existsByKakaoId(String kakaoId);
 }
