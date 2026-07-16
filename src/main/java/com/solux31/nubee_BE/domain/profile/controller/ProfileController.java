b058e6514459a5fe0dda10e3cbe77bf86cb8b5d0
yumin222 <ynssung1117@gmail.com>
2026-07-16 17:48:14 +0900

    docs: Tag 및 SecurityRequirement ì어노테이션 추가

--- src/main/java/com/solux31/nubee_BE/domain/profile/controller/ProfileController.java
+++ src/main/java/com/solux31/nubee_BE/domain/profile/controller/ProfileController.java
@@ -8,11 +8,15 @@ import com.solux31.nubee_BE.global.apiPayload.ApiResponse;
 import com.solux31.nubee_BE.global.security.entity.AuthUser;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "프로필", description = "프로필 조회 및 설정 관련 API")
+@SecurityRequirement(name = "JWT TOKEN")
 @RestController
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
