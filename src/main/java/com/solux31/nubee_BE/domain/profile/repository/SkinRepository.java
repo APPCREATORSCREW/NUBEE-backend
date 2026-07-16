e951b65dda86a2ecedbabadc96857ae47efb5ec0
yumin222 <ynssung1117@gmail.com>
2026-07-16 20:48:00 +0900

    SkinRepository에 findByRequiredLevel 메서드 추가

--- src/main/java/com/solux31/nubee_BE/domain/profile/repository/SkinRepository.java
+++ src/main/java/com/solux31/nubee_BE/domain/profile/repository/SkinRepository.java
@@ -3,5 +3,8 @@ package com.solux31.nubee_BE.domain.profile.repository;
 import com.solux31.nubee_BE.domain.profile.entity.Skin;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface SkinRepository extends JpaRepository<Skin, Long> {
+    Optional<Skin> findByRequiredLevel(int requiredLevel);
 }
