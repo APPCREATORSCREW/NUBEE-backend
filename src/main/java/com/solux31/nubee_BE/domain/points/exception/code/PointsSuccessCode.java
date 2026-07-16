df529c695d5e6724e0c065bfea80202b6786cc1f
yumin222 <ynssung1117@gmail.com>
2026-07-16 19:55:29 +0900

    PointHistory, PointsRepository, PointsResDTO, PointsConverter, Exception관련 코드 구현 완료

--- src/main/java/com/solux31/nubee_BE/domain/points/exception/code/PointsSuccessCode.java
+++ src/main/java/com/solux31/nubee_BE/domain/points/exception/code/PointsSuccessCode.java
@@ -1,4 +1,16 @@
 package com.solux31.nubee_BE.domain.points.exception.code;
 
-public enum PointsSuccessCode {
+import com.solux31.nubee_BE.global.apiPayload.code.BaseSuccessCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum PointsSuccessCode implements BaseSuccessCode {
+    POINT_FETCH_SUCCESS(HttpStatus.OK, "POINT200", "포인트 조회 성공");
+
+    private final HttpStatus status;
+    private final String code;
+    private final String message;
 }
