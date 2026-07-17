4580d1886f4cc3c0d4b1e1cf24f044af9e35ed95
daeunkim701 <lionboot2025@gmail.com>
2026-07-17 19:59:04 +0900

    fix: 코드리뷰 - N+1 쿼리 방지를 위한 Fetch Join 적용

--- src/main/java/com/solux31/nubee_BE/domain/words/repository/UserKeywordRepository.java
+++ src/main/java/com/solux31/nubee_BE/domain/words/repository/UserKeywordRepository.java
@@ -4,11 +4,13 @@ import com.solux31.nubee_BE.domain.words.entity.Keyword;
 import com.solux31.nubee_BE.domain.words.entity.mapping.UserKeyword;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserKeywordRepository extends JpaRepository<UserKeyword, Long> {
 
     // 유저의 단어 목록 조회
+    @EntityGraph(attributePaths = {"keyword"})
     List<UserKeyword> findByUserId(Long userId);
 
     // 특정 유저의 특정 단어 조회
