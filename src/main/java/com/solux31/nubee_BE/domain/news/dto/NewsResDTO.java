ffd0f01224390876c4c55510685689d7e6594071
daeunkim701 <lionboot2025@gmail.com>
2026-07-21 00:58:56 +0900

    feat: 응답 DTO 생성

---
+++ src/main/java/com/solux31/nubee_BE/domain/news/dto/NewsResDTO.java
@@ -0,0 +1,26 @@
+package com.solux31.nubee_BE.domain.news.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class NewsResDTO {
+
+    private String username;
+    private List<KeywordInfo> learnedKeywords;
+    private double keywordQuizAccuracy;
+    private double newsQuizAccuracy;
+
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    public static class KeywordInfo {
+        private String word;
+        private String originalUrl;
+    }
+}
