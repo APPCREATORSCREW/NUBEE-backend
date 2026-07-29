9b6d160314f75102f5e481238a2c1f74ed23f889
nylee0116 <specialnylee@gmail.com>
2026-07-29 14:18:53 +0900

    [fix] scheduler를 위해 한국 시간 기준 타임존 설정 #50

--- src/main/java/com/solux31/nubee_BE/NubeeBeApplication.java
+++ src/main/java/com/solux31/nubee_BE/NubeeBeApplication.java
@@ -1,14 +1,22 @@
 package com.solux31.nubee_BE;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.TimeZone;
+
 @EnableScheduling
 @SpringBootApplication
 @EnableAsync
 public class NubeeBeApplication {
+
+	@PostConstruct
+	public void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(NubeeBeApplication.class, args);
